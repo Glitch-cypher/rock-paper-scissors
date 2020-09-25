dfe07752992c91de17ed4b58b45f7fc1899d52d8
= <=>
2020-09-25 10:04:53 +0100

    task 1 if statements complete

---
+++ main.js
@@ -0,0 +1,11 @@
+let pM = "";
+let cM = "";
+
+if(pM === "rock" && cM === "scissors" || pM === "scissors" && cM === "paper" || pM === "paper" && cM === "rock") {
+
+} else if(cM === "rock" && pM === "scissors" || cM === "scissors" && pM === "paper" || cM === "paper" && pM === "rock") {
+
+} else if(cM === "rock" && pM === "rock" || cM === "scissors" && pM === "scissors" || cM === "paper" && pM === "paper") {
+  
+}
+
