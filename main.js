b86f8af61ffbe6788b0c74b084b0a35c9337f220
= <=>
2020-09-25 10:13:00 +0100

    task 2 complete

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 let pM = "";
 let cM = "";
 
+function getWinner (pM,cM){
+
 if(pM === "rock" && cM === "scissors" || pM === "scissors" && cM === "paper" || pM === "paper" && cM === "rock") {
-
+return 1;
 } else if(cM === "rock" && pM === "scissors" || cM === "scissors" && pM === "paper" || cM === "paper" && pM === "rock") {
-
+return -1;
 } else if(cM === "rock" && pM === "rock" || cM === "scissors" && pM === "scissors" || cM === "paper" && pM === "paper") {
-  
+  return 0;
 }
+}
+let result = getWinner("paper", "rock");
 
