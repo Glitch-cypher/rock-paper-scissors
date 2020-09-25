111909558a491d4382a5dd1078f1f2b66489b07b
= <=>
2020-09-25 10:47:33 +0100

    Task 4 complete

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-let cM = "rock";
+let cM = randomMove();
 let pM = prompt("Make your move!");
 
 function getWinner (pM,cM){
-
 if(pM === "rock" && cM === "scissors" || pM === "scissors" && cM === "paper" || pM === "paper" && cM === "rock") {
     alert("1");
 } else if(cM === "rock" && pM === "scissors" || cM === "scissors" && pM === "paper" || cM === "paper" && pM === "rock") {
@@ -10,7 +9,22 @@ if(pM === "rock" && cM === "scissors" || pM === "scissors" && cM === "paper" || 
 } else if(cM === "rock" && pM === "rock" || cM === "scissors" && pM === "scissors" || cM === "paper" && pM === "paper") {
     alert("0");
 }
+console.log(cM)
 }
 let result = getWinner(pM, cM);
 
 //
+function randomMove() {
+let roundNum = Math.round(Math.random()*2);
+if(roundNum === 0) {
+  return 'rock';
+} else if(roundNum === 1) {
+  return 'paper';
+} else if(roundNum === 2) {
+  return 'scissors';
+}
+
+
+}
+
+
