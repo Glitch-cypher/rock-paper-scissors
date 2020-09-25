bf35340ce016e390593ae819ad34cf386ceb3faf
= <=>
2020-09-25 11:45:23 +0100

    Task 6

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 let cM = randomMove();
 let pM = "";
+let wins = 0;
+let loss = 0;
+let draw = 0;
+let games = 0;
 
 
-
-while(confirm('still playing')){
+while(confirm('Ready for rock, paper, scissors?')){
   pM = prompt("Make your move!");
   cM = randomMove();
-  console.log(randomMove());
+  console.log(cM);
   getWinner(pM, cM);
+  alert(`wins: ${wins},\n losses:${loss},\n draws:${draw},\n games played:${games}`);
 }
 
 
 function getWinner (pM,cM){
 if(pM === "rock" && cM === "scissors" || pM === "scissors" && cM === "paper" || pM === "paper" && cM === "rock") {
+    wins ++;
     alert("1");
 } else if(cM === "rock" && pM === "scissors" || cM === "scissors" && pM === "paper" || cM === "paper" && pM === "rock") {
+    loss ++;
     alert ("-1");
 } else if(cM === "rock" && pM === "rock" || cM === "scissors" && pM === "scissors" || cM === "paper" && pM === "paper") {
+    draw ++;
     alert("0");
+}else{
+    alert('Dumbass, enter the right thing!');
 }
 // console.log(cM)
-
+games++;
 
 }
 //
